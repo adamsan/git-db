d762c196ce73c2a40a09ae0db87bf3044b6e85c0
Adam Szentirmai-Schon <adamsan3@gmail.com>
2020-11-08 22:29:19 +0100

    remove deprecated install_steps.bat

--- install_steps.bat
+++
@@ -1,8 +0,0 @@
- git config --global init.templatedir %userprofile%/.git-templates
- mkdir %userprofile%\.git-templates\hooks
-
-echo #!/bin/sh > %userprofile%\.git-templates\hooks\post-commit
-echo # Log last 5 commits on commit >> %userprofile%\.git-templates\hooks\post-commit
-echo git lg -5 >> %userprofile%\.git-templates\hooks\post-commit
-echo #echo "USING GITDB..." >> %userprofile%\.git-templates\hooks\post-commit
-
