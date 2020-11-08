f005b17476eba0a025c631a4f876c8d78b8dd00c
Adam Szentirmai-Schon <adamsan3@gmail.com>
2020-11-08 22:12:56 +0100

    complete help text (favor command)

--- src/main/kotlin/hu/adamsan/gitdb/commands/Help.kt
+++ src/main/kotlin/hu/adamsan/gitdb/commands/Help.kt
@@ -11,7 +11,9 @@ class Help {
 			$name init - initializes the tool - scans all drives, ignores hidden directories
 			$name init quick - initializes the tool by scanning the parents of the repos already stored in db
 			$name list - lists all git repositories
-			$name cd <project_id> - changes directory to repository's directory
+			$name cd <id> - changes directory to repository's directory
+			$name favor [id] - mark repository as favorite (repo identified by ID or by current path in the command line)
+			$name unfavor [id] - unmark repository as favorite
 
 			$name update <project_id | 0>- updates git repo on current directory - used by git hooks
 		""".trimIndent())
