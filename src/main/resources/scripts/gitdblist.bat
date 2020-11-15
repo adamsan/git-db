adc8d3b55ccdcd01f84a712bad44d83cd2baff27
Adam Szentirmai-Schon <adamsan3@gmail.com>
2020-11-15 12:39:34 +0100

    show commits in gitdb ls command

--- src/main/resources/scripts/gitdblist.bat
+++ src/main/resources/scripts/gitdblist.bat
@@ -1,5 +1,5 @@
 @echo off
 
-sqlite3  -column -header  %userprofile%/.git-db/.repos.db "select id, name, path, iif(favorite, '*', '') as favorite,  date(last_committed/1000, 'unixepoch', 'localtime') as last_commit_date, (strftime('%%s','now') - last_committed/1000)/60/60/24 as days_ago, iif(has_remote, '*', '') as has_remote from repo "
+sqlite3  -column -header  %userprofile%/.git-db/.repos.db "select id, name, path, iif(favorite, '*', '') as favorite, commits, date(last_committed/1000, 'unixepoch', 'localtime') as last_commit_date, (strftime('%%s','now') - last_committed/1000)/60/60/24 as days_ago, iif(has_remote, '*', '') as has_remote from repo "
 
 @echo on
